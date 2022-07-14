80efa6014547cf7749c98c5b0560c0fe34c3e6e4
bastianwalter <bastian.walter@tum.de>
2022-07-14 15:34:16 +0200

    591

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,20 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else if (query.contains("which of the following numbers is the largest: 79, 68, 591, 50")) {
-            return "591";
+        } else if (query.contains("which of the following numbers is the largest")) {
+            String[] a = query.split(":");
+            String numbers = a[2];
+            String[] ints = numbers.split(", ");
+            int[] res = new int[ints.length];
+            for (int i = 0; i < ints.length; ++i) {
+                res[i] = Integer.parseInt(ints[i]);
+            }
+            int b;
+            int max = res[0];
+            for (b = 1; b < res.length; b++)
+                if (res[b] > max)
+                    max = res[b];
+            return String.valueOf(max);
         } else {
             return "";
         }
